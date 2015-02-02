20b448ac6d7adf322b39f85a6763930ea003fe99
Johannes Böhm <polyhickup@gmail.com>
2015-02-02 21:56:03 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
 var ViewModel = function(){
+
+	this.currentCat = ko.observable( new Cat() );
 	
+	this.incrementCounter = function(){
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	}
+
+}
+
+var Cat = function() {
 	this.clickCount = ko.observable(0);
-	this.level = ko.observable("newborn");
+	this.title = ko.observable("newborn");
 	this.levelsArray = ko.observableArray();
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('https://d13yacurqjgara.cloudfront.net/users/285475/screenshots/1910598/bad_snap.gif');
 	this.imgAttribution = ko.observable('Bad Snap');
 
-	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
+	this.title = ko.computed(function(){
+		var title;
+		var clicks = this.clickCount();
 
-		switch (this.clickCount()) {
-			case 2: 
-			this.level("teen");
-			break;
+		switch (clicks) {
+			case 0:
+				title= "newborn";
+				return title;
+				break;
+			case 1: 
+				title ="teen";
+				return title;
+				break;
 		}
+	}, this);
 
-	}
-
+	this.nicknames = ko.observableArray([
+			{name: "Lilly"},
+			{name: "Lucy"},
+			{name: "Nora"}
+		]);
 }
 
 ko.applyBindings(new ViewModel());
